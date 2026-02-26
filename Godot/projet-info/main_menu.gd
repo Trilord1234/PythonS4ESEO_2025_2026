3a29752a8929e4327d1b3870338cb6d8f9a239c7
Trilord <tristan0.bernard@gmail.com>
2026-02-26 08:26:59 +0100

    Godot UI.
    Mon avancement de l'UI via Godot. Donc on a le main screen, avec les boutons qui marche des particules trop styler et de la musique avec un pop-up d'option avec lequel on peut modifier le son. (On a aussi les Artworks des planètes)

---
+++ Godot/projet-info/main_menu.gd
@@ -0,0 +1,30 @@
+extends Node2D
+var particule = preload("res://particule_manager.tscn")
+var time = 0.0
+var delay = 0.01
+
+func _ready():
+	$"Option Pop-up/OptionPopUp".visible = false
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
+
+func _on_option_pressed():
+	$"Option Pop-up/OptionPopUp".visible = true
+
+func _on_back_pressed():
+	$"Option Pop-up/OptionPopUp".visible = false
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _process(delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		time += delta
+		if time >= delay:
+			var clone = particule.instantiate()
+			clone.global_position = get_global_mouse_position()
+			add_child(clone)
+			time = 0.0
+	else:
+		time = delay
