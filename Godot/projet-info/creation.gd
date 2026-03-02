babf9db7f9c585f622c7c123d97b8885778e7f5a
Trilord <tristan0.bernard@gmail.com>
2026-03-02 10:47:39 +0100

    Compteur de planète
    Un petit compteur de planète

--- Godot/projet-info/creation.gd
+++ Godot/projet-info/creation.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
 var planet_manager = preload("res://planete_manager.tscn")
+var count = 0
 
 func _ready() :
 	randomize()
 
 func _on_generation_pressed():
+	count += 1
+	$"Background Manager/Info/NbPLanet".text = "NB Planet = " + str(count)
 	var new_planet = planet_manager.instantiate()
 	var box = $"Background Manager/PlaneteBox/HitBox/HitBox_Box"
 	new_planet.limit_x = box.size.x
