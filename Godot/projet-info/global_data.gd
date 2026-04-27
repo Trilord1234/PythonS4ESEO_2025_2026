e3497127afa7876967e36e551e2314b4f2a42573
BERNARD Tristan <tristan0.bernard@gmail.com>
2026-04-27 17:11:52 +0200

    Update Test .exe
    Teste pour la transition en .exe du projet Godot

--- Godot/projet-info/global_data.gd
+++ Godot/projet-info/global_data.gd
@@ -10,3 +10,19 @@ func erase_python_memory():
 	add_child(requeste_clear)
 	requeste_clear.request_completed.connect(func(_result, _response_code, _headers, _body): requeste_clear.queue_free())
 	requeste_clear.request(url, entetes, HTTPClient.METHOD_POST, "{}")
+
+
+var server_pid = 0
+func _ready():
+	var chemin_dossier = OS.get_executable_path().get_base_dir()
+	var chemin_serveur = chemin_dossier + "/Serveur_Tako_Space.exe"
+	if OS.has_feature("editor"):
+		print("Mode Éditeur : Pense à lancer le serveur Python manuellement !")
+		return
+	server_pid = OS.create_process(chemin_serveur, [])
+	print("Serveur Tako démarré avec le PID : ", server_pid)
+func _notification(what):
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		if server_pid > 0:
+			OS.kill(server_pid)
+		get_tree().quit()
